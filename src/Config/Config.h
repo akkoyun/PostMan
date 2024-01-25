e9e262936e2aeba0a8dfbce896f06baad1e20de0
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-01-25 09:35:34 +0300

    fix

--- src/Config/Config.h
+++ src/Config/Config.h
@@ -87,3 +87,13 @@
 #define Pack_FOTA_Download							91
 #define Pack_FOTA_Burn								99
 
+// Define PowerStat Commands
+#define Command_OK									200
+#define Command_NOK									201
+#define Command_Start								256
+#define Command_Stop								257
+#define Command_Update								262
+#define Command_Reset								999
+#define Command_Parameter							500
+#define Command_FOTA_Download						900
+#define Command_FOTA_Burn							901
