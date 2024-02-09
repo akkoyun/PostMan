96b96b014fdf37ccab3f825a937aa8547fa8e8fe
Mehmet Günce Akkoyun <akkoyun@me.com>
2024-02-09 13:39:54 +0300

    Library Update

--- src/PowerStatV4.h
+++ src/PowerStatV4.h
@@ -3772,30 +3772,6 @@ class Postman_PowerStatV4 : private AT_Command_Set, private GSM_Hardware, privat
 
 		}
 
-		// uint64 to String Converter Function
-		String uint64ToString(uint64_t input) {
-			
-			String result = "";
-			uint8_t base = 16;
-
-			do {
-				
-				char c = input % base;
-				input /= base;
-
-				if (c < 10)
-					c +='0';
-				else
-					c += 'A' - 10;
-			
-				result = c + result;
-
-			} while (input);
-
-			return result;
-
-		}
-
 		// IP Address to String Converter Function
 		String IPAddress_to_String(const uint8_t _IP_Address[4]) {
 
